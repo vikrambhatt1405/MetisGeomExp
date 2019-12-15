a575804382c767895c65a551847c3a741a08c944
vikrambhatt <vikrambhatt@iisc.ac.in>
2019-12-16 01:56:16 +0530

    First Commit

---
+++ CMakeFiles/MetisGeomExp.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mas/17/cdsbhat/MetisGeomExp/adjtocsr.cpp" "/home/mas/17/cdsbhat/MetisGeomExp/CMakeFiles/MetisGeomExp.dir/adjtocsr.cpp.o"
+  "/home/mas/17/cdsbhat/MetisGeomExp/main.cpp" "/home/mas/17/cdsbhat/MetisGeomExp/CMakeFiles/MetisGeomExp.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "/opt/mvapich2-1.8-r5423/gcc/include"
+  "/home/mas/17/cdsbhat/parmetis-4.0.3/include"
+  "/home/mas/17/cdsbhat/metis-5.1.0/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
